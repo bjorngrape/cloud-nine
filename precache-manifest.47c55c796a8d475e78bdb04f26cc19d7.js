a4595be312a44782e79edb4e3a36e42e501a93c8
bjorngrape <56294098+bjorngrape@users.noreply.github.com>
2020-10-23 14:39:30 +0200

    Updates

---
+++ precache-manifest.47c55c796a8d475e78bdb04f26cc19d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d999c4cd4a4c9c8bfe0354213ad7772",
+    "url": "/cloud-nine/index.html"
+  },
+  {
+    "revision": "72c8311819c62daf54a9",
+    "url": "/cloud-nine/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "fa05d7646048a308fe15",
+    "url": "/cloud-nine/static/css/main.1db2872a.chunk.css"
+  },
+  {
+    "revision": "72c8311819c62daf54a9",
+    "url": "/cloud-nine/static/js/2.181059cd.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/cloud-nine/static/js/2.181059cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa05d7646048a308fe15",
+    "url": "/cloud-nine/static/js/main.57f084f4.chunk.js"
+  },
+  {
+    "revision": "1df02fe5fe931e713865",
+    "url": "/cloud-nine/static/js/runtime-main.73281ce9.js"
+  }
+]);
